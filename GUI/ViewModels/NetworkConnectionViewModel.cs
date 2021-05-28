5c329a31142b4d29d6bad990e57752d06c62d47f
AndrryArmor <andrry1@gmail.com>
2021-05-29 02:44:32 +0300

    Add temporary projects to test client-server commnication

---
+++ GUI/ViewModels/NetworkConnectionViewModel.cs
@@ -0,0 +1,43 @@
+﻿using BelarusChess.UI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BelarusChess.GUI.ViewModels
+{
+    public class NetworkConnectionViewModel : ViewModelBase
+    {
+        private RelayCommand _createGameCommand;
+        private RelayCommand _joinGameCommand;
+
+        public bool IsCreateGameButtonEnabled { get; set; } = true;
+        public bool IsJoinGameButtonEnabled { get; set; } = true;
+        public string CreateGameMessage { get; set; }
+        public string JoinGameMessage { get; set; }
+
+        public RelayCommand CreateGameCommand
+        {
+            get
+            {
+                return _createGameCommand ?? (_createGameCommand = new RelayCommand(obj =>
+                {
+                    IsJoinGameButtonEnabled = false;
+
+                }));
+            }
+        }
+
+        public RelayCommand JoinGameCommand
+        {
+            get
+            {
+                return _joinGameCommand ?? (_joinGameCommand = new RelayCommand(obj =>
+                {
+                    IsCreateGameButtonEnabled = false;
+                }));
+            }
+        }
+    }
+}
